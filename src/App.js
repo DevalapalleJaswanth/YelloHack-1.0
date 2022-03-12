f9af21e63e9355299e8f088baa4593c1efb34238
DevalapalleJaswanth <83700845+DevalapalleJaswanth@users.noreply.github.com>
2022-03-12 07:02:16 +0530

    code for csv data generation is done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import "./styles.css";
 import React, { useState, useEffect } from "react";
-import { getUsers } from "./Services";
+import { getDate, getUsers } from "./Services";
 import { UserCard } from "../src/Components";
 export default function App() {
   const [users, setUsers] = useState([]);
+  const [csvData, setCsvData] = useState([]);
   useEffect(() => {
     async function data() {
       let data = await getUsers(100);
       console.log("users", data);
-      let temp = data ? (data.length > 0 ? setUsers([...data]) : "") : "";
-      // if (data && data.length > 0) {
-      //   setUsers([...data]);
-      // }
+      //let temp = data ? (data.length > 0 ? setUsers([...data]) : "") : "";
+      if (data && data.length > 0) {
+        setUsers([...data]);
+      }
       //console.log(users);
     }
     data();
   }, []);
+  //let csv = [];
+  function handleSelect(item) {
+    if (csvData.length === 0) {
+      let temp = {
+        name: item.name.title + ". " + item.name.first + " " + item.name.last,
+        gender: item.gender,
+        DOB: getDate(item),
+        email: item.email
+      };
+      // csv.push(temp);
+      setCsvData([...csvData, temp]);
+    } else {
+      let copy = [...csvData];
+      let count = 0;
+      copy.forEach((ele, i) => {
+        if (item.email === ele.email) {
+          copy.splice(i, 1);
+        } else {
+          count++;
+        }
+      });
+      if (count !== csvData.length) {
+        //console.log(copy);
+        setCsvData(copy);
+      } else {
+        let temp = {
+          name: item.name.title + ". " + item.name.first + " " + item.name.last,
+          gender: item.gender,
+          DOB: getDate(item),
+          email: item.email
+        };
+        setCsvData([...csvData, temp]);
+      }
+    }
+    //console.log("csvData", csvData);
+  }
+
   return (
     <div className="App">
-      {console.log(users)}
+      {console.log(users, csvData)}
       <div>
         <table>
           <thead>
@@ -29,10 +67,11 @@ export default function App() {
               <th>Email</th>
             </tr>
           </thead>
+
           <tbody>
             {users.length > 0 &&
               users.map((item, i) => (
-                <tr key={i} className="row">
+                <tr key={i} className="row" onClick={() => handleSelect(item)}>
                   <UserCard user={item} />
                 </tr>
               ))}
--- src/Services.js
+++ src/Services.js
@@ -6,3 +6,43 @@ export async function getUsers(num) {
   //console.log(user.results[0]);
   return user.results;
 }
+
+export function getDate(user) {
+  let DOB = user ? (user.dob ? user.dob.date : "") : "";
+
+  DOB = DOB.split("T")[0];
+
+  const getMonth = (x) => {
+    switch (x) {
+      case "01":
+        return "January";
+      case "02":
+        return "February";
+      case "03":
+        return "March";
+      case "04":
+        return "April";
+      case "05":
+        return "May";
+      case "06":
+        return "June";
+      case "07":
+        return "July";
+      case "08":
+        return "August";
+      case "09":
+        return "September";
+      case "10":
+        return "October";
+      case "11":
+        return "November";
+      case "12":
+        return "December";
+      default:
+        return "";
+    }
+  };
+  let m = getMonth(DOB.split("-")[1]);
+  DOB = m + " " + DOB.split("-")[2] + "," + DOB.split("-")[0];
+  return DOB;
+}
